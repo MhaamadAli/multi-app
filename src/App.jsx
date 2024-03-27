2406baa424ecb10cebabd0a54711dde4fe3c359f
MhaamadAli <m.alialaawar@gmail.com>
2024-03-27 02:27:52 +0200

    initialized project and updated folder structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Multi APP:</h1>
+      <ul>We have to create:
+        <li>Weather app to show the weather currently and for the next 3 days</li>
+        <li>Calculator: Display a calculator interface having the at least basic operators. The user can use the interface or the keyboard.</li>
+        <li>Sticky notes app: Display draggable sticky notes which can be removed, added and edited.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
